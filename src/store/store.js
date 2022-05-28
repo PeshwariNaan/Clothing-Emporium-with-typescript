1b5b5c3be7b745b44cbcd23aa9d3a8105c7b8046
J.D. Browne <PeshwariNaan01@proton.me>
2022-05-28 16:16:15 +0400

    Created my own logger middleware to see how middleware functions

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,29 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import logger from "redux-logger";
+//import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+// const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+//   Boolean
+// );
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log('type: ', action.type);
+  console.log('payload: ', action.payload);
+  console.log('currentState: ', store.getState());
+
+  next(action);
+
+  console.log('next state: ', store.getState());
+};
+
+const middleWares = [loggerMiddleware];
 
 const persistConfig = {
   key: "root",
