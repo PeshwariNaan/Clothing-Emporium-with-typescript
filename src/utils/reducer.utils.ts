7288a1262473f354b09af9babf0205d25ff09120
J.D. Browne <PeshwariNaan01@proton.me>
2022-06-03 11:33:58 +0400

    Added matcher functions to reducer utils so we can extract the type from the action creators.

--- src/utils/reducer.utils.ts
+++ src/utils/reducer.utils.ts
@@ -1,5 +1,30 @@
 import { AnyAction } from 'redux';
 
+//This is a matchable which we use to get around the issue with discriminanting unions
+//We want the reducer to throw an error if it doesn't return the default ...state.
+type Matchable<AC extends () => AnyAction> = AC & {
+  type: ReturnType<AC>['type'];
+  match(action: AnyAction): action is ReturnType<AC>;
+};
+
+export function withMatcher<AC extends () => AnyAction & { type: string }>(
+  actionCreator: AC
+): Matchable<AC>;
+
+export function withMatcher<
+  AC extends (...args: any[]) => AnyAction & { type: string }
+>(actionCreator: AC): Matchable<AC>;
+
+export function withMatcher(actionCreator: Function) {
+  const type = actionCreator().type;
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    },
+  });
+}
+
 export type ActionWithPayload<T, P> = {
   type: T;
   payload: P;
@@ -9,15 +34,18 @@ export type Action<T> = {
   type: T;
 };
 
-
 //Changing the create action function to include types: We use 'overloaded' functions to account for both actions w/ payload and
 //without payload
-export  function createAction<T extends string, P>(type: T, payload: P): ActionWithPayload<T, P>;
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P
+): ActionWithPayload<T, P>;
 
-export function createAction<T extends string>(type: T, payload: void): Action<T>
+export function createAction<T extends string>(
+  type: T,
+  payload: void
+): Action<T>;
 
 export function createAction<T extends string, P>(type: T, payload: P) {
   return { type, payload };
 }
-
-
