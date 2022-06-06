5bd84cab60d91a53812c56a561c43f2fafb859a5
J.D. Browne <PeshwariNaan01@proton.me>
2022-06-06 08:36:12 +0400

    Typed out form-input component and styles. Typed out sign-in form component.

--- src/components/form-input/form-input.styles.tsx
+++ src/components/form-input/form-input.styles.tsx
@@ -9,7 +9,11 @@ const shrinkLabelStyles = css`
   color: ${mainColor};
 `;
 
-export const FormInputLabel = styled.label`
+type FormInputLabel = {
+  shrink?: boolean;
+}
+
+export const FormInputLabel = styled.label<FormInputLabel>`
   color: ${subColor};
   font-size: 16px;
   font-weight: normal;
--- src/components/sign-in-form/sign-in-form.component.tsx
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, FormEvent, ChangeEvent } from 'react';
 import { useDispatch } from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
@@ -25,7 +25,7 @@ const SignInForm = () => {
     dispatch(googleSignInStart())
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
@@ -37,7 +37,7 @@ const SignInForm = () => {
     }
   };
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
 
     setFormFields({ ...formFields, [name]: value });
