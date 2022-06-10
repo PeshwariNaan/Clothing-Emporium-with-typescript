a50d7fb91141d1e5d59f5a1d0ecb07d443ced23b
J.D. Browne <PeshwariNaan01@proton.me>
2022-06-10 15:15:01 +0400

    Typed out product-card and spinner components.

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,9 +1,11 @@
+import { FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItemToCart } from '../../store/cart/cart.action';
 
 import { selectCartItems } from '../../store/cart/cart.selector';
 
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import { CategoryItem } from '../../store/categories/category.types';
 
 import {
   ProductCartContainer,
@@ -12,7 +14,11 @@ import {
   Price,
 } from './product-card.styles';
 
-const ProductCard = ({ product }) => {
+type ProductCardProps = {
+  product: CategoryItem;
+}
+
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
   const dispatch = useDispatch()
   const cartItems = useSelector(selectCartItems)
   const { name, price, imageUrl } = product;
